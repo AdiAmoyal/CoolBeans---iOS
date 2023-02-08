135db7bf94fc20b18f025e648018c69196b397bc
adiamoyal <adi.amoyal02@gmail.com>
2023-02-08 12:36:18 +0200

    Initial Commit

---
+++ CoolBeans/CoolBeansApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoolBeansApp.swift
+//  CoolBeans
+//
+//  Created by Adi Amoyal on 08/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct CoolBeansApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
